e2acf23dae81b475643552b1805b55f3a4700dec
Blockiy_Dmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2022-02-24 00:55:03 +0300

    Created logger service

---
+++ Stepometr/Stepometr/Stepometr/Service/LoggerService/ILogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepometer.Service.LoggerService
+{
+    public interface ILogService
+    {
+        public void Log(string message);
+        public void TrackException(Exception e);
+        public void TrackException(Exception e, string methodName);
+        public void TrackEvent(string message);
+        public void TrackResponse(HttpResponseMessage response);
+        public Task TrackResponseAsync(HttpResponseMessage response);
+    }
+}
